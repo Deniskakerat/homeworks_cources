500389529a10ab52234e1d0ef7d581b2255b7395
denslatv <denslats@gmail.com>
2022-09-23 16:31:03 +0300

    consolelog argument a=b

--- script/script3_1.js
+++ script/script3_1.js
@@ -1,6 +1,6 @@
-
-let a;
+let a,b;
     a=4;
-console.log("The argument is " + a);
-    a++;
-console.log("Now the argument is " + a);
+    b=-2;
+console.log("The argument is " + a + " another argument is " + b);
+    a=b;
+console.log("Now the arguments are " + a +" " + b);
