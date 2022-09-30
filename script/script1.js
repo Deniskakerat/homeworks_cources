286a02a0fc8b362e1e4e0d9941eb5594e0c2bbce
denslatv <denslats@gmail.com>
2022-09-30 22:38:29 +0300

    if/else/switch for fun

--- script/script1.js
+++ script/script1.js
@@ -1,18 +1,38 @@
-var x = 1;
-var y = 2;
-var res1 = x.toString()+y.toString() ; // Допишіть код, необхідно використовувати змінні x та y
-console.log(res1); // "12"
-console.log(typeof res1); // "string"
-
-var res2 = (x==x).toString()+y.toString() ; // Допишіть код, необхідно використовувати змінні x та y
-console.log(res2); // "true2"
-console.log(typeof res2); // "string"
-
-var res3 = (x!=y); // Допишіть код, необхідно використовувати змінні x та y
-console.log(res3); // true
-console.log(typeof res3); // "boolean"
-
-var res4 = x.NaN+y; // Допишіть код, необхідно використовувати змінні x та y
-console.log(res4); // NaN
-console.log(typeof res4); // "number"
-
+//Task1
+var x = 5;
+if(x>0){
+    x++;
+}else if(x<0){
+    x=x-2;
+}else{
+    x=10;
+}
+console.log(x);
+//Task2
+var a=3;
+(a==0)?console.log("нульове число"):
+    (a>0)?(a%2==0)?console.log("додатне парне"):console.log("додатне непарне"):
+        (a%2==0)?console.log("від’ємне парне число"): console.log("від’ємне непарне число");
+//Task3
+var a=[2,2,7,2];
+var chislo = a[0];
+var s;
+for(var i=1;i<a.length;i++){
+    if(a[i]==chislo){
+    }else{
+        s=i;
+    }
+}
+console.log(++s);
+//Task4
+var k=5;
+var str ;
+switch (k){
+    case 1:str = "погано";break;
+    case 2:str = "погано";break;
+    case 3:str = "задовільно";break;
+    case 4:str = "добре";break;
+    case 5:str = "відмінно";break;
+    default:str = "помилка";
+}
+console.log(str);
