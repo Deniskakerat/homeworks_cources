081509303d724cd3c30dd20c47efdc4008dd219c
denslatv <denslats@gmail.com>
2022-10-11 14:26:43 +0300

    function Expression hw10 add()

--- script/script4.js
+++ script/script4.js
@@ -1,21 +1,6 @@
 //Task1
-let side_square = 4;
-console.log("The area is " + Math.pow(side_square,2));
-//Task2
-let two_number = 36;
-let result =  (two_number-two_number%10)/10 ;
-console.log("The second number is " + two_number +" in the argument is " + result );
-console.log("The first number is " + two_number +" in the argument is " + two_number%10 );
-//Task3
-let a = 12, b=8;
-console.log((a > 2) );
-console.log((b <= 3) );
-//Task4
-let a1 = 3, b1=6, c1=4;
-console.log((a1 < b1 && b1 < c1) );
-//Task5
-let a2 = 9;
-console.log("is the number %2");
-console.log((a2%2===0));
-console.log("is the number %3");
-console.log((a2%3===0));
+var adding = function add(x, y) {
+    return x + y;
+}
+
+console.log(adding(12, 8));
