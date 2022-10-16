50d0de1c84001c8c619eb1910ce071303ed8017b
denslatv <denslats@gmail.com>
2022-10-16 18:02:40 +0300

    recurse function digitSum

--- script/script4.js
+++ script/script4.js
@@ -1,54 +1,22 @@
 //Task1
 "use strict";
+/*Напишіть рекурсивну функцію digitSum(k), яка знаходить суму цифр цілого числа k  без використання циклів.
+*/
+var sum = 0;
+var k = 121;
 
-function calc(x, y) {
-
-    var arr = [];
-    arr[0] = 2 * (x + y);
-    arr[1] = x * y;
-
-    function output() {
-        console.log("The numbers are " + x + " and " + y +
-            " perimetr is " + arr[0] + " the area is " + arr[1]);
+function digitSum(k) {
+    if(k!=0) {
+        var dob = k % 10;
+        k = k - dob;
+        k = k / 10;
+        sum = sum + dob;
+        return digitSum(Math.ceil(k));
     }
-
-    return output;
+    return 0;
 }
 
-var func = calc(5, 7);
-func();
+digitSum(k);
+console.log(Math.ceil(sum));
 
-//Task2
-var i = -1;
 
-function createArrayIterator(array) {
-    function returnarr() {
-        i++;
-        return arr[i];
-    }
-
-    return returnarr;
-
-}
-
-var arr = [5, 3, 7];
-var itr = createArrayIterator(arr);
-console.log(itr()); // 5
-console.log(itr()); // 3
-console.log(itr()); // 7
-console.log(itr()); // undefined
-
-//Task3
-var arr1 = [];
-
-for (var i = 0; i <= 2; i++) {
-    var inner = function () {
-        var index = i;
-        return function () {
-            console.log(index);
-        };
-    };
-    arr1[i] = inner();
-}
-arr1[0](); // 0
-arr1[arr1.length - 1](); // 2
