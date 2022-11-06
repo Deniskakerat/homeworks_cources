cd01ed37ff1b62132dd1d6fb60a5f9ee543e4400
denslatv <denslats@gmail.com>
2022-11-06 11:18:13 +0200

    Object function hw16

--- script/script4.js
+++ script/script4.js
@@ -1,28 +1,36 @@
 //Task1
 "use strict";
-/*
-Створіть об'єкт calculator, який буде мати методи:
 
-1. read() запитує два значення з допомогою prompt() і зберігає в властивостях об'єкта;
-2. sum() повертає суму цих двох значень;
-3. mul() повертає добуток цих двох значень.
-*/
-var calculator = {
-    a: 0,
-    b: 0,
-    read: function () {
-        this.a = Number(prompt("Enter a"));
-        this.b = Number(prompt("Enter b"));
-    },
-    sum: function () {
-        let s = this.a + this.b;
-        return s;
-    },
-    mul: function () {
-        return this.a * this.b;
-    }
-    // Ваш код
+/*
+Task 2.
+Напишіть функцію, яка приймає як параметр об'єкт salaries із зарплатами та повертає значення максимальної із зарплат.
+Код має працювати з різною кількістю зарплат в об'єкті.
+Приклади salaries у файлі task2.js
+ */
+var salaries1 = {
+    John: 100,
+    Bill: 300,
+    Mike: 250,
 };
-calculator.read();
-console.log(calculator.sum());
-console.log(calculator.mul());
+
+var salaries2 = {
+    Cris: 150,
+    Brain: 600,
+    John: 300,
+    Steve: 400,
+    Bill: 50,
+};
+
+function count(obj) {
+    let max = 0;
+    for (let key in obj) {
+        if (max <= obj[key]) {
+            max = obj[key];
+        }
+    }
+    return max;
+}
+
+console.log(count(salaries1));
+console.log(count(salaries2));
+
