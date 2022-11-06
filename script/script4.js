08736906b1bcb84e7a2f3fec9315c9564225017a
denslatv <denslats@gmail.com>
2022-11-06 16:58:04 +0200

    Object Function hw17

--- script/script4.js
+++ script/script4.js
@@ -1,6 +1,5 @@
 //Task1
 "use strict";
-
 /*
 Напишіть функцію mul(), яка приймає будь-яку кількість параметрів різного типу та повертає добуток параметрів типу Number.
 Якщо параметри Number відсутні, повертає число 0.
@@ -16,3 +15,25 @@ function mul (x,y){
 }
 console.log(mul(1, "str", 2, 3, true)); // 6
 console.log(mul(null, "str", false, true)); // 0
+//Task2
+/*
+Напишіть функцію mul(), яка приймає будь-яку кількість параметрів різного типу та повертає добуток параметрів типу Number.
+Якщо параметри Number відсутні, повертає число 0.
+ */
+var country = {
+    name: "Ukraine",
+    language: "ukrainian",
+    capital: {
+        name: "Kyiv",
+        population: 2907817,
+        area: 847.66,
+    },
+};
+function format(beginMsg, endMsg) {
+    console.log(beginMsg + this.name + endMsg);
+}
+format.call(country,"<",">"); // "<Ukraine>"
+format.apply(country,["[","]"]); // "[Ukraine]"
+format.call(country.capital,"\"","\""); // ""Kyiv""
+format.apply(country.capital,["", ""]); // "Kyiv"
+
