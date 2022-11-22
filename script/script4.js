72d226defa1b5781bcf3fb5ed73935297945e5e9
denslatv <denslats@gmail.com>
2022-11-22 23:05:03 +0200

    bind context

--- script/script4.js
+++ script/script4.js
@@ -1,65 +1,58 @@
 //Task1
 "use strict";
 /*
-Task  2
-Створіть тип об'єктів Суматор, з наступним функціоналом:
-1. Приватна властивість __firstNumber;
-2. Приватна властивість __secondNumber;
-3. Публічна властивість  result;
-4. Приватний метод __calc(), який записує в result суму властивостей __firstNumber та __secondNumber;
-5. Єдині методи геттер-сеттер для властивостей __firstNumber і __secondNumber, під час роботи методів
-у якості сеттерів, повинна відбуватися валідація (встановлювати можна лише значення типу Number),
-якщо валідація відбулась успішно, повинен викликатись метод __calc(). Виходить, що при успішній установці
-чи __firstNumber чи __secondNumber, відбувається перерахунок result.
+Task 1.
+Дано наступний код (файл task1.js):
+Прив'яжіть функцію format() до об'єкта user.
+Реалізуйте 2 версії поточного завдання за допомогою:
+1. Анонімну функцію обгортку;
+2. Метод bind().
  */
 
-function Sumator(number1, number2) {
-    this.result = +0;
-    this.__firstnumber = +0; // Приватна властивість
-    this.__secondNumber = +0; // Приватна властивість
-    if (this.numberValid(number1)) {
-        this.__firstnumber = number1;
-        this.__calc();
+var user = {
+    name: "Tom",
+    print: function (beginMsg, endMsg) {
+        console.log(beginMsg + " " + this.name + " " + endMsg);
     }
-    if (this.numberValid(number2)) {
-        this.__secondNumber = number2;
-        this.__calc();
-    }
+};
+function tomFormat(func) {
+    func();
+};
+tomFormat(function () {
+    user.print("<<<", ">>>");
+});
+
+var tomFormat1 = user.print.bind(user ,"<<<", ">>>");
+tomFormat(tomFormat1);
+/*Task 2.
+Дано наступний  код (файл task2.js):
+Використовуючи функцію mul() і каринг створіть дві функції doubleMul() і qadraMul(), які множать числа на 2 і 4 відповідно.
+ */
+function mul(a, b) {
+    return a * b;
 }
 
-Sumator.prototype.getfirstNumber = function () { // Публічний метод
-    return this.__firstnumber;
-};
-Sumator.prototype.setfirstNumber = function (number) { // Публічний метод
-    if (this.numberValid(number)) {
-        this.__firstnumber = number;
-        this.__calc();
-    }
-};
-Sumator.prototype.getsecondNumber = function () { // Публічний метод
-    return this.__secondNumber;
-};
-Sumator.prototype.setsecondNumber = function (number) { // Публічний метод
-    if (this.numberValid(number)) {
-        this.__secondNumber = number;
-        this.__calc();
-    }
-};
-Sumator.prototype.__calc = function () {
-    this.result = this.__firstnumber + this.__secondNumber;
-};
-// Приватний метод
-Sumator.prototype.numberValid = function (number1) {
-    if (typeof (number1) === "number") {
-        return true;
-    } else {
-        return false;
-    }
-};
+var doubleMul = mul.bind(null,2)
+var qudraMul = mul.bind(null,4)
+    console.log(doubleMul(5)); // 10
+console.log(qudraMul(5)); // 20
+/*
+Task 3.
+Використовуючи код із файлу (файл task3.js) Напишіть аналог методу bind().
+ */
 
-var task1 = new Sumator(9, 4);
-console.log(task1.result);
-task1.setfirstNumber("2");
-console.log(task1.result);
-task1.setfirstNumber(12);
-console.log(task1.result);
+function bind(func,context) {
+    return function() {
+        return func.apply(context);
+    };
+};
+function func() {
+    console.log(this.name + " - " + this.age + " " + this.surname);
+}
+var user = {
+    name: "Tom",
+    age: 20,
+    surname:"Redy"
+};
+var f = bind(func, user);
+f();
